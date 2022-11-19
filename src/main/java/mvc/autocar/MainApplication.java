d533d6f6162d82c62a235ce91728b363fb77b8d4
STILINSKI-ANAS <anastnx@gmail.com>
2022-11-19 21:32:11 +0100

    fix css, and add everything to search function

--- src/main/java/mvc/autocar/MainApplication.java
+++ src/main/java/mvc/autocar/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/View/Menu1.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/View/results.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         scene.getStylesheets().add(getClass().getResource("/assets/style1.css").toExternalForm());
         stage.setTitle("Hello!");
