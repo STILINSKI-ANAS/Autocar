799cb4a6594b82894ed7ccf54cfec2ab1a572893
hakeemkadim <hakeemkadim@gmail.com>
2022-11-19 20:50:32 +0100

    Agencies FXML Agencies Controller Agencies Class Changes in connection class

--- src/main/java/mvc/autocar/Model/Repository/connectivity/ConnectionClass.java
+++ src/main/java/mvc/autocar/Model/Repository/connectivity/ConnectionClass.java
@@ -1,10 +1,12 @@
 package mvc.autocar.Model.Repository.connectivity;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import mvc.autocar.Model.Agencies;
 
-public class ConnectionClass {
+import java.sql.*;
+
+public class ConnectionClass<connection> {
 public Connection connection;
     public Connection getConnection()
     {
@@ -23,5 +25,22 @@ public Connection connection;
             System.out.println("null");
         }
         return connection;
+    }
+
+    public  ObservableList<Agencies> getDataAgencies() {
+        Connection conn = getConnection();
+        ObservableList<Agencies> list = FXCollections.observableArrayList();
+
+        try{
+            PreparedStatement ps = conn.prepareStatement("select * from Agencies");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Agencies(rs.getInt("idAgencie"), rs.getString("nom"),rs.getString("adresse"), rs.getString("status"), rs.getInt("tel"), rs.getInt("ticketDisponible"), rs.getInt("ticketDisponible") ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+
     }
 }
